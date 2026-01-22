ccd771d96d1f561a17f07de7d5ce4e414eda8998
2bllikigai <kieuquangtruong2005tn@gmail.com>
2026-01-22 13:30:53 +0700

    Reset project, keep markdown only

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace EcoConnect_Hanoi.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- Service/IEmailSender.cs
+++
@@ -1,8 +0,0 @@
-// EcoConnect_Hanoi.Services/IEmailSender.cs
-namespace EcoConnect_Hanoi.Services
-{
-    public interface IEmailSender
-    {
-        Task SendEmailAsync(string email, string subject, string message);
-    }
-}
